acfc5533ebaa6344cee033dbdf4a571e66dfb02e
4ngry <4ngry24@gmail.com>
2022-12-07 12:10:53 +0200

    Added tqdm (loading/processing bar).

--- src/main.py
+++ src/main.py
@@ -4,13 +4,15 @@ from art import text2art
 from gtts import gTTS
 import pdfplumber
 from termcolor import colored
+from tqdm import tqdm as barlib
 
 
 def read_pdf(file_path: str):
     if Path(file_path).is_file() and Path(file_path).suffix == ".pdf":
         print(colored(text=f"[+] File found. Processing...", color="green"))
+
         with pdfplumber.PDF(open(file=file_path, mode="rb")) as pdf_file:
-            pages: list = [page.extract_text() for page in pdf_file.pages]
+            pages: list = [page.extract_text() for page in barlib(pdf_file.pages)]
 
         text: str = ''.join(pages)
         text: str = text.replace('\n', '')
@@ -25,6 +27,7 @@ def convert_to_mp3(text: str, file_path: str) -> dict:
         return {"status": "failure"}
     audio = gTTS(text=text, lang='en', slow=False)
     file_name = Path(file_path).stem
+    print(colored(text=f"[+] Saving file...", color="green"))
     audio.save(f"{file_name}.mp3")
 
     print(colored(text=f"[+][Success]: {file_name}.mp3 saved.", color="green"))
